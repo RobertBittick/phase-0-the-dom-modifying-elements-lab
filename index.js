55b7427257c0ce225e6d6ca16876853aae587619
Robert Bittick <robertbittick@gmail.com>
2022-10-07 19:12:54 -0700

    commit tree cbe7873938b3f5f7ae4e23f337d3e4bb772ce472

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const ul = document.getElementsByTagName('main')
+const byeBye = main.querySelector('main#main')
+main.remove('main')
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "Robert is the champion"
